d972b7f15d6ac6014a6ba57b983d7097e2932b0a
NickHallONBI <nicholas.hall@dtc.ox.ac.uk>
2019-09-10 17:34:21 +0100

    Added Fourier power metric measurement

--- microAO/aoMetrics.py
+++ microAO/aoMetrics.py
@@ -65,3 +65,35 @@ def measure_fourier_metric(image, wavelength=500 * 10 ** -9, NA=1.1,
 def measure_contrast_metric(image, **kwargs):
     return (np.max(image) - np.min(image))/(np.max(image) + np.min(image))
 
+def measure_fourier_power_metric(image, wavelength=500 * 10 ** -9, NA=1.1,
+                            pixel_size=0.1193 * 10 ** -6):
+    ray_crit_dist = (1.22 * wavelength) / (2 * NA)
+    ray_crit_freq = 1 / ray_crit_dist
+    max_freq = 1 / (2 * pixel_size)
+    freq_ratio = ray_crit_freq / max_freq
+    OTF_outer_rad = (freq_ratio) * (np.shape(image)[0] / 2)
+
+    im_shift = np.fft.fftshift(image)
+    tukey_window = tukey(im_shift.shape[0], .10, True)
+    tukey_window = np.fft.fftshift(tukey_window.reshape(1, -1) * tukey_window.reshape(-1, 1))
+    im_tukey = im_shift * tukey_window
+    fftarray = np.fft.fftshift(np.fft.fft2(im_tukey))
+
+    fftarray_sq_log = np.log(np.real(fftarray * np.conj(fftarray)))
+
+    cent2corner = np.sqrt(2 * ((image.shape[0] / 2) ** 2))
+    rad_to_corner = cent2corner - OTF_outer_rad
+    noise_corner_size = int(np.round(np.sqrt((rad_to_corner ** 2) / 2) * 0.9))
+    noise = (fftarray_sq_log[0:noise_corner_size, 0:noise_corner_size] +
+            fftarray_sq_log[0:noise_corner_size, -noise_corner_size:] +
+            fftarray_sq_log[-noise_corner_size:, 0:noise_corner_size] +
+            fftarray_sq_log[-noise_corner_size:, -noise_corner_size:]) / 4
+    threshold = np.mean(noise) * 1.125
+
+    ring_mask = make_ring_mask(np.shape(image),0.1 * OTF_outer_rad, OTF_outer_rad)
+    ramp_mask = np.sqrt(np.outer(np.linspace(-150,150,image.shape[0])**2,np.ones(image.shape[1]).T)+
+                        np.outer(np.ones(image.shape[0]), (np.linspace(-150,150,image.shape[1])**2).T))
+    freq_above_noise = (fftarray_sq_log > threshold) * ring_mask * ramp_mask
+    metric = np.sum(freq_above_noise)
+    return metric
+
