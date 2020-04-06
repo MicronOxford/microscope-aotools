cc7f5582338d2bd5d8b5747ba309acaa36691704
NickHallONBI <nicholas.hall@dtc.ox.ac.uk>
2020-04-06 13:11:55 +0100

    Current version is 1.1.1+dev

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ manifest_files = [
 
 setuptools.setup(
   name = "microscope-aotools",
-  version = "1.1.1",
+  version = "1.1.1+dev",
   description = "An extensible microscope adaptive optics use.",
   license = "GPL-3.0+",
 
